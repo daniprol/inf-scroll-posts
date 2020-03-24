593274cc37c2bf0ed89103e9e6376c791e9b179a
daniprol <daniprol@hotmail.com>
2020-03-24 21:07:45 +0100

    Loading more posts after reaching bottom

--- script.js
+++ script.js
@@ -45,3 +45,26 @@ async function showPosts() {
 
 // Show initial posts:
 showPosts();
+
+// Show loader & fetch more posts:
+function showLoading() {
+  loading.classList.add("show");
+
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    // console.log("Now");
+    showLoading();
+  }
+});
